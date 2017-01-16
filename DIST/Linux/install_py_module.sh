c87598fc3c34057007c467215286175683ad53c3
quaxquax <henning.dekant@gmail.com>
2017-01-15 19:33:11 -0500

    Updated Linux install script

--- DIST/Linux/install_py_module.sh
+++ DIST/Linux/install_py_module.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-command -v conda-env >/dev/null 2>&1 || { printf >&2 "Command line tool 'conda-env' is not available.\nThis modul requires Anaconda Python 3.5.\nPlease check if you have Anaconda Python installed!\n\n"; exit 1; }
+# For Linux
+CUNCSDMODULE=cuncsd.cpython-35m-x86_64-linux-gnu.so  
+
+command -v ls >/dev/null 2>&1 || { printf >&2 "Command line tool 'conda-env' is not available.\nThis modul requires Anaconda Python 3.5.\nPlease check if you have Anaconda Python installed, and on the $PATH variable!\n\n"; exit 1; }
 
 
 EXPORTFILE=/tmp/exportfile${RANDOM}
@@ -19,35 +22,85 @@ conda-env list | {
 
   # This won't work without the braces.
   echo $CondaPath > $EXPORTFILE
+  exit 1;
 }
 CondaPath=$(cat $EXPORTFILE)
-read -p "Found Anaconda Python install path '$CondaPath'. Is this correct? " -n 1 -r
+read -p "Found Anaconda Python install path '$CondaPath'. Is this correct? [Y|n]" -n 1 -r
 echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ ! $REPLY =~ ^[Nn]$ ]];
 then
+    echo $CondaPath
     if [ -d "$CondaPath/bin" ]; then
 	# Control will enter here if $DIRECTORY exists.
-	echo "Confirmed Python 'bin' directory."
+	echo "Confirmed Python '$CondaPath/bin' directory."
     else
 	echo "ERROR: Python 'bin' directory is missing."
 	exit 1;
     fi
     if [ -d "$CondaPath/lib" ]; then
 	# Control will enter here if $DIRECTORY exists.
-	echo "Confirmed Python 'lib' directory."
+	echo "Confirmed Python '$CondaPath/lib' directory."
     else
-	echo "ERROR: Python 'lib' directory is missing."
+	echo "ERROR: Python '$CondaPath/lib' directory is missing."
 	exit 1;
     fi
-    FullPyVersion=$((python -V) 2>&1)
-    echo "blub"
+    FullPyVersion=$(($CondaPath/bin/python -V) 2>&1)
     PyVStr=${FullPyVersion%:: *} 
-    echo $PyVStr
     if [ "$PyVStr" == "Python 3.5.2 " ]; then 
        echo "Found matching $PyVStr."
     else
-	echo "Python 3.5.2 required but detected $PyVStr."
-	exit 1;
+       echo "WARNING: This module has only been tested with Anaconda Python 3.5.2 but detected $PyVStr(!)"
     fi
-    exit 1;
+    [ -w $CondaPath/lib/python3.5 ] && echo "Confirmed that $CondaPath/lib/python3.5 is writeable." ||
+	    {
+		echo "ERROR: No write permission for $CondaPath/lib/python3.5"
+		exit 1
+	    }
+    cp -R rect $CondaPath/lib/python3.5
+    cp $CUNCSDMODULE $CondaPath/lib/python3.5
+    [ -w ./lib ] && cp ./lib/* $CondaPath/lib
+    echo "**** Successfully installed the cuncsd module to $CondaPath/lib/python3.5 ****"
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
